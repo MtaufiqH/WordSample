978964b6012abe265315b762d805427fbe551e42
mtaufiqh62@gmail.com <123darussalam>
2019-05-09 15:28:10 +0800

    create viewModel Class

---
+++ app/src/main/java/com/example/taufiq/wordsample/WordViewModel.java
@@ -0,0 +1,11 @@
+package com.example.taufiq.wordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class WordViewModel extends AndroidViewModel {
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
