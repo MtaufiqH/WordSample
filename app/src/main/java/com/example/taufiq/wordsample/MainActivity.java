1aa0854a551e8752bdda67cd72f1e32d4d04f81a
mtaufiqh62@gmail.com <123darussalam>
2019-05-10 10:05:45 +0800

    Create adapter to Make RecyclerView

--- app/src/main/java/com/example/taufiq/wordsample/MainActivity.java
+++ app/src/main/java/com/example/taufiq/wordsample/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.taufiq.wordsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init the RecyclerView
+        init();
+    }
+
+    void init(){
+        RecyclerView recyclerView = findViewById(R.id.rv_main);
+        WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/taufiq/wordsample/WordListAdapter.java
@@ -0,0 +1,66 @@
+package com.example.taufiq.wordsample;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    private final LayoutInflater minflater;
+    private List<Word> mWords; // cache copy of words
+
+
+     WordListAdapter(Context context) {
+        minflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = minflater.inflate(R.layout.recyclerview_item, viewGroup, false);
+        return new WordViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder wordViewHolder, int position) {
+        if (mWords != null) {
+            Word current = mWords.get(position);
+            wordViewHolder.words.setText(current.getaWord());
+        } else {
+            // cover the case of data not being ready yet.
+            wordViewHolder.words.setText(R.string.no_word);
+        }
+    }
+
+    void setWords(List<Word> words) {
+        mWords = words;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWords != null) {
+            return mWords.size();
+        } else {
+            return 0;
+        }
+
+    }
+
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView words;
+
+        WordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            words = itemView.findViewById(R.id.word_id);
+        }
+    }
+}
