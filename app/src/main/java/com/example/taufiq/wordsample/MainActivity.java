5a5f369ebb5eb9f64f163ab6a90f4e1937663c61
mtaufiqh62@gmail.com <123darussalam>
2019-05-10 10:52:15 +0800

    populate data into room

--- app/src/main/java/com/example/taufiq/wordsample/MainActivity.java
+++ app/src/main/java/com/example/taufiq/wordsample/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.taufiq.wordsample;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // init the RecyclerView
-        init();
-    }
-
-    void init(){
         RecyclerView recyclerView = findViewById(R.id.rv_main);
-        WordListAdapter adapter = new WordListAdapter(this);
+        final WordListAdapter adapter = new WordListAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable List<Word> words) {
+                // update cached copy of the words in the adapter
+                adapter.setWords(words);
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/taufiq/wordsample/WordDatabase.java
+++ app/src/main/java/com/example/taufiq/wordsample/WordDatabase.java
@@ -1,9 +1,12 @@
 package com.example.taufiq.wordsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1,exportSchema = false)
 public abstract class WordDatabase extends RoomDatabase {
@@ -27,12 +30,51 @@ public abstract class WordDatabase extends RoomDatabase {
                             // if no Migration object.
                             // Migration is not part of this practical.
                             .fallbackToDestructiveMigration()
+                            .addCallback(sroomCallback)
                             .build();
                 }
             }
         }
 
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sroomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+
+        }
+
+
+    };
+
+    /**
+    * Populate database in the background thread
+    * */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
     }
 
 }
