253a5fc48ec0b2609bf6bb80588088292cd3fbbf
mtaufiqh62@gmail.com <123darussalam>
2019-05-10 09:09:47 +0800

    menerapkan view model

--- app/src/main/java/com/example/taufiq/wordsample/WordViewModel.java
+++ app/src/main/java/com/example/taufiq/wordsample/WordViewModel.java
@@ -2,10 +2,28 @@ package com.example.taufiq.wordsample;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository wordRepo;
+    private LiveData<List<Word>> allWords;
+
+
     public WordViewModel(@NonNull Application application) {
         super(application);
+        wordRepo = new WordRepository(application);
+        allWords = wordRepo.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords(){
+        return allWords;
+    }
+
+    public void insert(Word word){
+        wordRepo.insert(word);
     }
 }
